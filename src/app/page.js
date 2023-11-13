6e131c56774ea0be206b281825fb882f92db1fc9
rmadiraju2026 <146482099+rmadiraju2026@users.noreply.github.com>
2023-11-13 14:34:50 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,13 @@ export default function Home() {
   return (
     <script>
       import { sql } from '@vercel/postgres';
+      CREATE TABLE backend101 (
+          Column1 int,
+          Column2 varchar(255),
+          Column3 varchar(255),
+          Column4 varchar(255),
+          Column5 varchar(255)
+      );
     </script>
   )
 }
