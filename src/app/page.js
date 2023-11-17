152396809855c6fe7c4f236796db4d59173b5a47
rmadiraju2026 <146482099+rmadiraju2026@users.noreply.github.com>
2023-11-16 18:44:06 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -43,30 +43,29 @@ export async function POST(req, res) {
 export default function Home() {
   
   return (
-    <!DOCTYPE HTML>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <title>SQL Data Interaction</title>
-        </head>
-        <body>
-          <h1>Data</h1>
-          <ul id="dataList"></ul>
-        
-          <h2>Add New Data</h2>
-          <form id="dataForm">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required><br><br>
-            
-            <label for="age">Age:</label>
-            <input type="number" id="age" name="age" required><br><br>
-        
-            <button type="submit">Add Data</button>
-          </form>
-        
-          <script src="script.js"></script>
-        </body>
-      </html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>SQL Data Interaction</title>
+      </head>
+      <body>
+        <h1>Data</h1>
+        <ul id="dataList"></ul>
+      
+        <h2>Add New Data</h2>
+        <form id="dataForm">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required><br><br>
+          
+          <label for="age">Age:</label>
+          <input type="number" id="age" name="age" required><br><br>
+      
+          <button type="submit">Add Data</button>
+        </form>
+      
+        <script src="script.js"></script>
+      </body>
+    </html>
     <script>
       import { sql } from '@vercel/postgres';
       CREATE DATABASE db;
