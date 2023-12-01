3044a6cf05c50d2d801a42c3e45610de14f2aadb
rmadiraju2026 <146482099+rmadiraju2026@users.noreply.github.com>
2023-11-30 18:35:04 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-// server.js
-
-import { NextResponse } from "next/server";
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-
-// Open SQLite database connection
-async function openDB() {
-  return open({
-    filename: './your_database.db',
-    driver: sqlite3.Database,
-  });
-}
-
-export async function GET(req, res) {
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request) {
   try {
-    const db = await openDB();
-    const data = await db.all('SELECT * FROM YourTable');
-    await db.close();
-    return NextResponse.json(data, { status: 200 });
+    const result =
+      await sql`CREATE TABLE data ( Name varchar(255), Age int );`;
+    return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
-export async function POST(req, res) {
-  try {
-    const bod = req?.body;
-    if (bod?.name && bod?.age) {
-      const db = await openDB();
-      await db.run('INSERT INTO YourTable (name, age) VALUES (?, ?)', [bod.name, bod.age]);
-      const newData = await db.all('SELECT * FROM YourTable');
-      await db.close();
-      return NextResponse.json(newData, { status: 201 });
-    }
-    return NextResponse.json({ message: 'Please provide both name and age.' }, { status: 400 });
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
 
