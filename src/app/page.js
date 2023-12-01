96671ffe08cdf843096a76c20aaf4616cc14e72d
rmadiraju2026 <146482099+rmadiraju2026@users.noreply.github.com>
2023-11-30 18:36:54 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,8 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
- 
-export async function GET(request) {
-  try {
-    const result =
-      await sql`CREATE TABLE data ( Name varchar(255), Age int );`;
-    return NextResponse.json({ result }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
-  }
-}
+// server.js
 
+import { NextResponse } from "next/server";
 export default function Home() {
   
   return (
